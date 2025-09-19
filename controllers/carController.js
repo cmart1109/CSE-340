e69d9dfe052f8dd7fe6f6dde8e3517a2892db805
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-09-19 11:50:36 -0600

    second commit on assignment week 3

--- controllers/carController.js
+++ controllers/carController.js
@@ -7,15 +7,17 @@ carCont.buildCarDetails = async function(req,res,next) {
         const car_id = req.params.inv_id
         const data = await carModel.getCarDetails(car_id)
         if (data.length === 0) {
-            return res.status(404).setd("Car not found")
+            return res.status(404).send("Car not found")
         }
-        const CarHTML = await utilities.buildcarDetails(data[0])
-        res.render("car/details", {
+        const carHtml = await utilities.buildcarDetails(data[0])
+        res.render("inventory/details", {
             title: `${data[0].inv_make} ${data[0].inv_model}`,
-            CarHTML
+            carHtml
         })
     } catch (err) {
         next(err)
     }
 
 }
+
+module.exports = carCont;
--- models/car-model.js
+++ models/car-model.js
@@ -2,7 +2,8 @@ const pool = require("../database/");
 
 async function getCarDetails(carId) {
     const query = `SELECT * FROM public.inventory WHERE inv_id = $1`;
-    const param = carId;
+    const param = [carId];
+    console.log("Running car details query:", query, param);
     const result = await pool.query(query, param);
     return result.rows; 
 }
