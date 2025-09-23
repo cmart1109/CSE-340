7e3a0944082e9073cceeb80952ece096b6fc046a
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-09-23 10:06:26 -0600

    changes addittionally

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 /* ***********************
  * Require Statements
  *************************/
+const session = require("express-session")
+const pool = require("./database/")
 const express = require("express")
 const inventoryRoutes = require("./routes/inventoryRoutes")
 const expressLayouts = require("express-ejs-layouts")
@@ -17,6 +19,20 @@ const carRoutes = require("./routes/carRoute")
 /* ***********************
  * View Engine and Templates
  *************************/
+/*
+Middleware
+*/
+app.use(session({
+    store: new (require('connect-pg-simple')(session)) ({
+      createTableIfMissing: true,
+      pool,
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    name: 'SessionId' 
+}))
+
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout","./layouts/layout")
